ae143fc6567b4ba4247c394cd1a2a13022497d17
Rupert Parry <ruparry@gmail.com>
2019-06-29 11:38:21 +1000

    Added hyper.js & wrote syncing script

---
+++ .hyper.js
@@ -0,0 +1,76 @@
+module.exports = {
+  config: {
+    // Choose either "stable" for receiving highly polished,
+    // or "canary" for less polished but more frequent updates
+    updateChannel: 'stable',
+
+    // default font size in pixels for all tabs
+    fontSize: 14,
+
+    lineHeight: 1.3,
+
+    // font family with optional fallbacks
+    fontFamily: '"Operator Mono", Consolas, "Lucida Console", monospace',
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BLOCK',
+
+    // set to true for blinking cursor
+    cursorBlink: false,
+    
+    backgroundColor: '#000',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: '',
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: '',
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: '12px 14px',
+
+    shell: '',
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
+
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false
+
+  },
+
+  plugins: [
+    'hypercwd',
+    'hyper-flat',
+    'hyper-font-smoothing',
+    'gitrocket',
+    'hypercwd',
+    'hyperline',
+    'hyper-final-say'
+  ],
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: [],
+
+  keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+  }
+};
+
+
---
+++ sync.fish
@@ -0,0 +1,3 @@
+rm -r ~/.config; ln -sv ~/.dotfiles/.config ~/.config
+rm ~/.vimrc; ln -sv ~/.dotfiles/.vimrc ~/.vimrc
+rm ~/.hyper.js; ln -sv ~/.dotfiles/.hyper.js ~/.hyper.js
